03d43bfdf5aab93cc728da3c26c2533f11e4ca5c
Matthieu Haefele <matthieu.haefele@maisondelasimulation.fr>
2016-11-29 14:42:21 +0100

    Fix compilation bug in 1_example

--- 1_example/compile.sh
+++ 1_example/compile.sh
@@ -2,4 +2,3 @@ HDF5_INC=-I/usr/include
 HDF5_LIB="-L/usr/lib -lhdf5 -lm"
 gcc $HDF5_INC  -std=c99 gen_data_grid.c -o gen_data_grid $HDF5_LIB
 gcc $HDF5_INC  -std=c99 gen_data_unstructured.c -o gen_data_unstructured $HDF5_LIB
-gcc $HDF5_INC  -std=c99 gen_data_vector.c -o gen_data_vector $HDF5_LIB
